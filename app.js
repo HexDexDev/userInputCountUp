35623fa4a93c518048a916a4d876c35a5ea896aa
HexDexDev <138143730+HexDexDev@users.noreply.github.com>
2024-06-19 22:32:21 -0300

    fixed code

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 // Pede um número ao usuário
-let numeroDoUsuario = prompt("Digite um número");
+let numeroMaximo = prompt("Digite um número para a contagem progregressiva:");;
 
 // Inicializa o contador a partir do zero
 let contador = 0;
 
 // Cria uma regra que se o número do usuário for maior que 0
-if (numeroDoUsuario >= 0) {
-// Loop while que continua enquanto contador for menor ou igual ao número do usuário
-    while (contador <= numeroDoUsuario) {
+while (contador <= numeroMaximo) {
+    // Loop while que continua enquanto contador for menor ou igual ao número do usuário
+    console.log(contador);
         // Mostra o valor atual do contador
         alert(contador);
         // Incrementa o contador
-        contador++;
+        contador++
     }
